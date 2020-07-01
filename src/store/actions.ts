bf3837e253b21ecbed664a91b90d9b9fc1c73a56
leegeunhyeok <dev.ghlee@gmail.com>
2020-07-01 21:00:36 +0900

    UPDATE: Base code structure
    - Add Header component
    - Add react-redux

---
+++ src/store/actions.ts
@@ -0,0 +1,12 @@
+import * as types from './types';
+import { Product } from '../models';
+
+export interface AddCartAction {
+  type: typeof types.ADD_CART
+  payload: Product
+};
+
+export const addCart = (product: Product): AddCartAction => ({
+  type: types.ADD_CART,
+  payload: product
+});
---
+++ src/store/reducres.ts
@@ -0,0 +1,26 @@
+import { Product } from '../models';
+import * as types from './types';
+import { AddCartAction } from './actions';
+
+export interface CartState {
+  products: Product[]
+}
+
+const initialState: CartState = {
+  products: []
+}
+
+const cartReducer = (state = initialState, action: AddCartAction): CartState => {
+  switch (action.type) {
+    case types.ADD_CART: 
+      return {
+        ...state,
+        products: [...state.products, action.payload]
+      };
+
+    default:
+      return state;
+  };
+}
+
+export default cartReducer;
