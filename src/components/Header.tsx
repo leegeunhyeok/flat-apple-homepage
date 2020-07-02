fa401322edd61b4cbfe6d5796464f22c836b3f27
leegeunhyeok <dev.ghlee@gmail.com>
2020-07-02 17:38:35 +0900

    UPDATE: Default header style/layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import logo from '../assets/logo.svg';
 import './Header.scss';
 
 function Header() {
   return (
-    <header>Header</header>
+    <header className="Header">
+      <div className="Header-logo">
+        <img src={logo} alt="logo"/>
+      </div>
+      <div className="Header-menu">
+        <ul>
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+        </ul>
+      </div>
+    </header>
   )
 }
 
