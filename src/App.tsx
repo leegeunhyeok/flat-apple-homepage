09eddfc133facd2ff791910001869fd26731038a
leegeunhyeok <dev.ghlee@gmail.com>
2020-07-05 21:08:52 +0900

    ADD: react-transition-group / Add Loading component transition effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
+import { SetWidthAction, setWidth } from './store/actions';
 import './App.scss';
 
-import { CommonState } from './store/reducers';
-import { SetWidthAction, setWidth } from './store/actions';
-
+import Loading from './components/Loading';
 import Header from './components/Header';
 
 interface RootProps {
@@ -14,6 +14,7 @@ interface RootProps {
 
 function App(props: RootProps) {
   const { setWidth } = props;
+  const [loading, setLoadState] = useState(true);
   const getTestEl = () => {
     const style = {
       display: 'inline-block',
@@ -29,23 +30,26 @@ function App(props: RootProps) {
   window.addEventListener('resize', () => {
     setWidth(window.innerWidth);
   });
-  setWidth(window.innerWidth);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div className="App">
       <Header />
       {/* For test */}
       {getTestEl()}
+      <CSSTransition in={loading} timeout={500} unmountOnExit classNames="fade">
+        <Loading onload={() => setLoadState(false)} />
+      </CSSTransition>
     </div>
   );
 }
-
-const mapStateToProps = (state: CommonState) => ({
-  width: state.width,
-});
 
 const mapDispatchToProps = (dispatch: Dispatch<SetWidthAction>) => ({
   setWidth: (width: number) => dispatch(setWidth(width)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(null, mapDispatchToProps)(App);
